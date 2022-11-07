52775c2932200d373cd828c1ee9b81b63721dcbb
Carol Cuesta <cuestalz@mit.edu>
2022-11-07 16:52:56 -0500

    nested sampling seems to work

--- scripts/inference/infer.py
+++ scripts/inference/infer.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     nested = Nested.from_config("configs/infer.yaml")
     t0 = time.time()
     print(f"Fitting parameters {nested.param_names}")
-    nested(log_dir="results/", slice_steps=None, num_live_points=400)
+    nested()
     print("Fitting took = ", time.time() - t0)
