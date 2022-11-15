901096068c9ac86051a8697cee0b2af6ef8752a9
Carol Cuesta <cuestalz@mit.edu>
2022-11-15 15:02:55 -0500

    new interface for inference

--- sunbird/inference/inference.py
+++ sunbird/inference/inference.py
@@ -1,90 +1,180 @@
 from abc import ABC, abstractmethod
-import pandas as pd
-from scipy import stats
 from pathlib import Path
 import importlib
 import numpy as np
+import pandas as pd
 import yaml
-import json
 import matplotlib.pyplot as plt
-import sys
-
-# TODO:
-# - Run mcmc with only HOD parameters
-# - Run mcmc s > 0, s > 25
+from typing import Dict, Optional, List
+import xarray as xr
+from sunbird.models import Predictor
 
 DATA_PATH = Path(__file__).parent.parent.parent / "data/"
 
 class Inference(ABC):
     def __init__(
         self,
-        theory_model,
-        observation,
-        covariance_matrix,
-        priors,
-        fixed_parameters,
-        s_min: float,
-        quintiles,
-        output_dir,
-        device="cpu",
+        theory_model: Predictor,
+        observation: np.array,
+        covariance_matrix: np.array,
+        priors: Dict,
+        fixed_parameters: Dict[str, float],
+        filters: Dict,
+        output_dir: Path,
+        device: str ="cpu",
     ):
         self.theory_model = theory_model
         self.observation = observation
         self.covariance_matrix = covariance_matrix
-        self.inverse_covariance_matrix = self.invert_covariance(
-            covariance_matrix=self.covariance_matrix,
-        )
+        #self.inverse_covariance_matrix = self.invert_covariance(
+        #    covariance_matrix=self.covariance_matrix,
+        #)
         self.priors = priors
         self.n_dim = len(self.priors)
         self.fixed_parameters = fixed_parameters
         self.device = device
-        self.s_min = s_min
         self.param_names = list(self.priors.keys())
-        self.quintiles = quintiles
+        self.filters = filters
         self.output_dir = Path(output_dir)
+
+    @classmethod
+    def from_abacus_config(
+        cls,
+        path_to_config: Path,
+        device: str ="cpu",
+    )->"Inference":
+        with open(path_to_config, "r") as f:
+            config = yaml.safe_load(f)
+        filters = config['filters']
+        observation = cls.get_observation_for_abacus(
+            cosmology= config['data']['cosmology'],
+            hod_idx= config['data']['hod_idx'],
+            statistic=config['data']['summary'],
+            filters=filters,
+        )
+        parameters = cls.get_parameters_for_abacus(
+            cosmology= config['data']['cosmology'],
+            hod_idx= config['data']['hod_idx'],
+        )
+        fixed_parameters = {}
+        for k in config['fixed_parameters']:
+            fixed_parameters[k] = parameters[k]
+        #covariance_matrix = cls.get_covariance_data(
+        #    path_to_cov=config['data']["path_to_covariance_data"],
+        #    filters=filters,
+        #)
+        covariance_matrix = None
+        theory_model = cls.get_theory_model(
+            config["theory_model"],
+            filters,
+        )
+        parameters_to_fit = [p for p in theory_model.parameters if p not in fixed_parameters.keys()]
+        priors = cls.get_priors(config["priors"], parameters_to_fit)
+        return cls(
+            theory_model=theory_model,
+            observation=observation,
+            filters=filters,
+            covariance_matrix=covariance_matrix,
+            fixed_parameters=fixed_parameters,
+            priors=priors,
+            output_dir = config['inference']['output_dir'],
+            device=device,
+        )
+    
+    @classmethod
+    def get_observation_for_abacus(
+        cls,
+        cosmology: int,
+        hod_idx: int,
+        statistic: str,
+        filters: Dict,
+    )->np.array:
+        if statistic == 'density_split':
+            data = np.load(
+                DATA_PATH
+                / f"full_ap/clustering/ds/ds_cross_xi_smu_zsplit_Rs20_c{str(cosmology).zfill(3)}_ph000.npy",
+                allow_pickle=True,
+            ).item()
+            quintiles = range(5)
+        elif statistic == 'tpcf':
+            data = np.load(
+                DATA_PATH
+                / f"full_ap/clustering/xi_smu/xi_smu_c{str(cosmology).zfill(3)}_ph000.npy",
+                allow_pickle=True,
+            ).item()
+        else:
+            raise ValueError(f'{statistic} is not implemented!')
+        s = data["s"]
+        multipoles = range(3)
+        data = data['multipoles']
+        data = np.mean(data,axis=1)
+        data = data[hod_idx]
+        if statistic == 'density_split':
+            data = xr.DataArray(
+                data, 
+                dims=("quintiles", "multipoles", "s"), 
+                coords={
+                "quintiles": list(quintiles),
+                "multipoles": list(multipoles),
+                "s": s,
+                },
+            )
+            return data.sel(
+                quintiles=filters['quintiles'],
+                multipoles=filters['multipoles'],
+                s=slice(filters['s_min'],filters['s_max']),
+            ).values.reshape(-1)
+        elif statistic == 'tpcf':
+            data = xr.DataArray(
+                data, 
+                dims=("multipoles", "s"), 
+                coords={
+                "multipoles": list(multipoles),
+                "s": s,
+                },
+            )
+            return data.sel(
+                multipoles=filters['multipoles'],
+                s=slice(filters['s_min'],filters['s_max']),
+            ).values.reshape(-1)
+
+
+    @classmethod
+    def get_parameters_for_abacus(
+        cls,
+        cosmology: int,
+        hod_idx: int,
+    ):
+        params_dict = dict(
+            pd.read_csv(
+                DATA_PATH
+                / f"full_ap/cosmologies/AbacusSummit_c{str(cosmology).zfill(3)}_hod1000.csv"
+            ).iloc[hod_idx]
+        )
+        params_dict['alpha_sat'] = params_dict['alpha_s']
+        return params_dict
 
     @classmethod
     def from_config(
         cls,
-        path_to_config,
-        device="cpu",
-    ):
+        path_to_config: Path,
+        device: str ="cpu",
+    )->"Inference":
         with open(path_to_config, "r") as f:
             config = yaml.safe_load(f)
-        data_config = config["data"]
+        filters = config['filters']
         observation = cls.get_observation(
-            path_to_observation=data_config["path_to_observation"],
-            quintiles=data_config["quintiles"],
-            s_min=data_config["s_min"],
+            path_to_observation=config['data']["path_to_observation"],
+            filters=filters,
         )
-        fixed_parameters = cls.get_fixed_parameters(
-            path_to_fixed_parameters=data_config["path_to_fixed_parameters"]
-        )
-        fixed_parameters = {
-            k: fixed_parameters[k] for k in data_config["fixed_parameters"]
-        }
+        fixed_parameters = config["fixed_parameters"]
         covariance_matrix = cls.get_covariance_data(
-            path_to_cov=data_config["path_to_covariance_data"],
-            quintiles=data_config["quintiles"],
-            s_min=data_config["s_min"],
+            path_to_cov=config['data']["path_to_covariance_data"],
+            filters=filters,
         )
-        if config["inference"]["add_emulation_error"]:
-            print('adding emulator error')
-            cov_perf = cls.get_covariance_perf(
-                path_to_cov=data_config["path_to_covariance_perf"],
-                quintiles=data_config["quintiles"],
-                s_min=data_config["s_min"],
-            )
-            cov_test = cls.get_covariance_test(
-                path_to_cov=data_config["path_to_covariance_test"],
-                quintiles=data_config["quintiles"],
-                s_min=data_config["s_min"],
-            )
-            cov_emu = cov_perf + cov_test
-            covariance_matrix =+ cov_emu
-            # print(np.sqrt(np.diag(covariance_matrix)))
         theory_model = cls.get_theory_model(
             config["theory_model"],
+            filters=filters,
         )
         parameters_to_fit = [p for p in theory_model.parameters if p not in fixed_parameters.keys()]
         priors = cls.get_priors(config["priors"], parameters_to_fit)
@@ -94,14 +184,12 @@ class Inference(ABC):
             covariance_matrix=covariance_matrix,
             fixed_parameters=fixed_parameters,
             priors=priors,
-            device=device,
-            s_min=data_config["s_min"],
-            quintiles=data_config["quintiles"],
             output_dir = config['inference']['output_dir'],
+            device=device,
         )
 
     @classmethod
-    def get_priors(cls, prior_config, parameters_to_fit):
+    def get_priors(cls, prior_config: Dict[str,Dict], parameters_to_fit: List[str])->Dict:
         distributions_module = importlib.import_module(prior_config.pop("stats_module"))
         prior_dict = {}
         for param in parameters_to_fit:
@@ -112,64 +200,31 @@ class Inference(ABC):
         return prior_dict
 
     @classmethod
-    def get_fixed_parameters(cls, path_to_fixed_parameters):
-        with open(path_to_fixed_parameters, "r") as fp:
-            fixed_params = json.load(fp)
-        return fixed_params
-
-    @classmethod
-    def get_observation(cls, path_to_observation, quintiles, s_min):
-        data = np.load(
-            path_to_observation,
-        )
-        s = data[0]
-        multipoles = data[1:, :]
-        multipoles = multipoles[quintiles]
-        return multipoles[:, s > s_min].reshape(-1)
+    def get_observation(cls, path_to_observation: Path, filters: Dict)->np.array:
+        return 
 
     @classmethod
     def get_covariance_data(cls, path_to_cov, s_min: float, quintiles):
         data = np.load(path_to_cov, allow_pickle=True).item()
         s = data["s"]
         multipoles = []
-        for q in quintiles:
-            multipoles.append(data["multipoles"][:, q, 0][:, s > s_min])
+        if quintiles is not None:
+            for q in quintiles:
+                multipoles.append(data["multipoles"][:, q, 0][:, s > s_min])
+        else:
+            multipoles.append(data["multipoles"][:, 0][:, s > s_min])
         multipoles = np.array(multipoles)
         multipoles = np.transpose(multipoles, axes=(1, 0, 2))
         multipoles = multipoles.reshape((len(multipoles), -1))
         return np.cov(multipoles.T)
 
     @classmethod
-    def get_covariance_test(cls, path_to_cov, s_min: float, quintiles, volume_rescaling=64):
-        data = np.load(path_to_cov, allow_pickle=True).item()
-        s = data["s"]
-        multipoles = []
-        for q in quintiles:
-            multipoles.append(data["multipoles"][:, q, 0][:, s > s_min])
-        multipoles = np.array(multipoles)
-        multipoles = np.transpose(multipoles, axes=(1, 0, 2))
-        multipoles = multipoles.reshape((len(multipoles), -1))
-        return np.cov(multipoles.T) / volume_rescaling
-
-    @classmethod
-    def get_covariance_perf(cls, path_to_cov, s_min: float, quintiles):
-        data = np.load(path_to_cov, allow_pickle=True).item()
-        s = data["s"]
-        multipoles = []
-        for q in quintiles:
-            qq = q - 1 if q > 2 else q
-            multipoles.append(data["multipoles"][:, qq][:, s > s_min])
-        multipoles = np.array(multipoles)
-        multipoles = np.transpose(multipoles, axes=(1, 0, 2))
-        multipoles = multipoles.reshape((len(multipoles), -1))
-        return np.cov(multipoles.T)
-
-    @classmethod
-    def get_theory_model(cls, theory_config):
+    def get_theory_model(cls, theory_config, filters):
         module = theory_config.pop("module")
         class_name = theory_config.pop("class")
         return getattr(importlib.import_module(module), class_name)(
-            **theory_config["params"]
+            **theory_config["params"],
+            **filters,
         )
 
     @abstractmethod
@@ -211,14 +266,14 @@ class Inference(ABC):
         return -0.5 * np.einsum('ki,ji', diff, right)[:,0]
 
     def sample_from_prior(
-        self,
+        self, 
     ):
         params = {}
         for param, dist in self.priors.items():
             params[param] = dist.rvs()
         for p, v in self.fixed_parameters.items():
             params[p] = v
-        return params, self.theory_model(params, quintiles=self.quintiles, s_min=self.s_min)
+        return params, self.theory_model(params, filters=self.filters)
 
     def get_model_prediction(
         self,
@@ -229,8 +284,7 @@ class Inference(ABC):
             params[fixed_param] = self.fixed_parameters[fixed_param] 
         return self.theory_model(
             params,
-            s_min=self.s_min,
-            quintiles=self.quintiles,
+            filters=self.filters,
         )
 
     def get_model_prediction_vectorized(
@@ -247,6 +301,5 @@ class Inference(ABC):
         out = self.theory_model.get_for_batch(
             params,
             s_min=self.s_min,
-            quintiles=self.quintiles,
         )
         return out.reshape((len(parameters), -1))
