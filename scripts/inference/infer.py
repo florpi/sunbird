62723ba6e57ac3a925e7a36f9d44469970b8fb38
Carol Cuesta <cuestalz@mit.edu>
2022-11-15 15:07:26 -0500

    up to date

--- scripts/inference/infer.py
+++ scripts/inference/infer.py
@@ -2,7 +2,7 @@ from sunbird.inference import Nested
 import time
 
 if __name__ == "__main__":
-    nested = Nested.from_config("configs/infer_tpcf.yaml")
+    nested = Nested.from_abacus_config("configs/infer.yaml")
     t0 = time.time()
     print(f"Fitting parameters {nested.param_names}")
     nested()
