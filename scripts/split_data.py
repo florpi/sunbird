fixed covariance

scripts/split_data.py
@@ -14,10 +14,16 @@ def read_params(cosmo_idx, dataset):
 
 def read_tpcf_multipoles(
     cosmo_idx,
+    dataset,
     multipole=0,
 ):
+    if dataset == 'combined':
+        s, same = read_multipoles(cosmo_idx, dataset='same_hods', multipole=multipole) 
+        s, different = read_multipoles(cosmo_idx, dataset='different_hods', multipole=multipole) 
+        return s, np.concatenate((same, different))
+
     data = np.load(
-        f"../data/clustering/xi_smu/xi_smu_c{cosmo_idx}_ph000.npy",
+        f"../data/clustering/{dataset}/xi_smu/xi_smu_c{cosmo_idx}_ph000.npy",
         allow_pickle=True,
     ).item()
     return data['s'], np.mean(data["multipoles"], axis=1)[:, multipole]
@@ -57,85 +63,108 @@ def store_summaries(s, parameters, multipoles, path_to_store):
     with open(path_to_store, "w") as fd:
         json.dump(summary, fd)
 
-def store_params(idx_list, output_path, dataset):
+def read_params_for_stage(idx_list, dataset):
     params = []
     for idx in idx_list:
         params.append(read_params(idx, dataset))
     params = np.array(params)
-    params = params.reshape(-1, params.shape[-1])
-    np.save(output_path, params)
-    #f"../data/datasets/{dataset}/train_params.npy", params[train_idx].reshape(-1, params.shape[-1]))
+    return params.reshape(-1, params.shape[-1])
 
-
-if __name__ == "__main__":
-    quintiles = [0, 1, 3, 4]
-    dataset = 'same_hods'
-    with open('../data/train_test_split.json') as f:
-        train_test_split = json.load(f)
-    for stage in ['train', 'test', 'val']:
-        np.save(
-            idx_list=train_test_split[stage],
-            output_path=f"../data/datasets/{dataset}/{stage}_params.npy", 
-            dataset=dataset,
-        )
-
-    '''
+def read_tpcf_for_stage(idx_list, dataset,):
     multipoles = []
-    for idx in cosmo_idx:
+    for idx in idx_list:
         combined_multi = []
         for multipole in [0, 1]:
-            s, multi = read_tpcf_multipoles(idx, multipole=multipole)
+            s, multi = read_tpcf_multipoles(idx, multipole=multipole, dataset=dataset)
             combined_multi.append(multi)
         combined_multi = np.hstack(combined_multi)
         multipoles.append(combined_multi)
     multipoles = np.array(multipoles)
     s = np.array(list(s) + list(s))
-    np.save(
-        f"../data/train_tpcf.npy",
-        multipoles[train_idx].reshape(-1, multipoles.shape[-1]),
+    multipoles = multipoles.reshape((-1, multipoles.shape[-1]))
+    return s, multipoles
+
+if __name__ == "__main__":
+    quintiles = [0, 1, 3, 4]
+    filter_type = ['tophat', 'gaussian']
+    corr_types = ['auto', 'cross']
+    dataset = 'different_hods'
+    with open('../data/train_test_split.json') as f:
+        train_test_split = json.load(f)
+    train_params = read_params_for_stage(
+        train_test_split['train'],
+        dataset=dataset
     )
+    np.save(
+        f"../data/datasets/{dataset}/train_params.npy", 
+        train_params,
+    )
+    for stage in ['test', 'val']:
+        params = read_params_for_stage(train_test_split[stage], dataset=dataset)
+        np.save(
+            f"../data/datasets/{dataset}/{stage}_params.npy", 
+            params,
+        )
+    s, train_multipoles = read_tpcf_for_stage(train_test_split['train'], dataset=dataset)
+    np.save(
+        f"../data/datasets/{dataset}/train_tpcf.npy",
+        train_multipoles
+    )
+    np.save(f'../data/s.npy', np.unique(s))
     store_summaries(
         s,
-        params[train_idx].reshape(-1, params.shape[-1]),
-        multipoles[train_idx].reshape(-1, multipoles.shape[-1]),
-        f"../data/train_tpcf_summary.json",
+        train_params,
+        train_multipoles,
+        f"../data/datasets/{dataset}/train_tpcf_summary.json",
     )
-    np.save(
-        f"../data/test_tpcf.npy",
-        multipoles[test_idx].reshape(-1, multipoles.shape[-1]),
-    )
-    np.save(
-        f"../data/val_tpcf.npy",
-        multipoles[val_idx].reshape(-1, multipoles.shape[-1]),
-    )
-    '''
 
-    for quintile in quintiles:
-        multipoles = []
-        for idx in cosmo_idx:
-            combined_multi = []
-            for multipole in [0, 1]:
-                s, multi = read_multipoles(idx, dataset=dataset,quintile=quintile, multipole=multipole)
-                combined_multi.append(multi)
-            combined_multi = np.hstack(combined_multi)
-            multipoles.append(combined_multi)
-        multipoles = np.array(multipoles)
-        s = np.array(list(s) + list(s))
+    for stage in ['test', 'val']:
+        s, multipoles = read_tpcf_for_stage(train_test_split[stage], dataset=dataset)
         np.save(
-            f"../data/datasets/{dataset}/train_ds{quintile}.npy",
-            multipoles[train_idx].reshape(-1, multipoles.shape[-1]),
+            f"../data/datasets/{dataset}/{stage}_tpcf.npy",
+            multipoles 
         )
-        np.save(
-            f"../data/datasets/{dataset}/test_ds{quintile}.npy",
-            multipoles[test_idx].reshape(-1, multipoles.shape[-1]),
-        )
-        np.save(
-            f"../data/datasets/{dataset}/val_ds{quintile}.npy",
-            multipoles[val_idx].reshape(-1, multipoles.shape[-1]),
-        )
-        store_summaries(
-            s,
-            params[train_idx].reshape(-1, params.shape[-1]),
-            multipoles[train_idx].reshape(-1, multipoles.shape[-1]),
-            f"../data/datasets/{dataset}/train_ds{quintile}_summary.json",
-        )
+
+    for corr_type in corr_types:
+        for quintile in quintiles:
+            s, train_multipoles_quintile = read_multipoles_for_stage(
+                train_test_split['train'], 
+                dataset=dataset,
+                quintile=quintile,
+            )
+            store_summaries(
+                s,
+                train_params,
+                train_multipoles_quintile,
+                f"../data/datasets/{dataset}/train_ds{quintile}_{corr_type}_summary.json",
+            )
+'''
+            multipoles = []
+            for idx in cosmo_idx:
+                combined_multi = []
+                for multipole in [0, 1]:
+                    s, multi = read_multipoles(idx, dataset=dataset,quintile=quintile, multipole=multipole)
+                    combined_multi.append(multi)
+                combined_multi = np.hstack(combined_multi)
+                multipoles.append(combined_multi)
+            multipoles = np.array(multipoles)
+            s = np.array(list(s) + list(s))
+            np.save(
+                f"../data/datasets/{dataset}/train_ds{quintile}.npy",
+                multipoles[train_idx].reshape(-1, multipoles.shape[-1]),
+            )
+            np.save(
+                f"../data/datasets/{dataset}/test_ds{quintile}.npy",
+                multipoles[test_idx].reshape(-1, multipoles.shape[-1]),
+            )
+            np.save(
+                f"../data/datasets/{dataset}/val_ds{quintile}.npy",
+                multipoles[val_idx].reshape(-1, multipoles.shape[-1]),
+            )
+            store_summaries(
+                s,
+                params[train_idx].reshape(-1, params.shape[-1]),
+                multipoles[train_idx].reshape(-1, multipoles.shape[-1]),
+                f"../data/datasets/{dataset}/train_ds{quintile}_summary.json",
+            )
+'''

scripts/split_into_traintestval.py
@@ -6,19 +6,17 @@ import random
 dataset = 'different_hods'
 
 n_derivative_grid = 27
-cosmo_idx = list(np.arange(100, 100 + n_derivative_grid)) + list(
-    np.arange(130, 181)
-)
+derivative_grid = list(np.arange(100,100 + n_derivative_grid))
+lhc_grid = list(np.arange(130,181))
 percent_val = 0.1
 percent_test = 0.1
-n_samples = len(cosmo_idx)
-idx = n_derivative_grid + np.arange(n_samples - n_derivative_grid)
-random.shuffle(idx)
+random.shuffle(lhc_grid)
+n_samples = len(lhc_grid)
 n_val = int(np.floor(percent_val * n_samples))
 n_test = int(np.floor(percent_test * n_samples))
-val_idx = idx[:n_val]
-test_idx = idx[n_val : n_val + n_test]
-train_idx = list(idx[n_val + n_test :]) + list(range(n_derivative_grid))
+val_idx = lhc_grid[:n_val]
+test_idx = lhc_grid[n_val : n_val + n_test]
+train_idx = list(lhc_grid[n_val + n_test :]) + derivative_grid
 
 split_dict = {
     'train': [int(idx) for idx in train_idx],

sunbird/inference/inference.py
@@ -9,6 +9,7 @@ from typing import Dict, Optional, List
 import xarray as xr
 from sunbird.models import Predictor
 from sunbird.covariance import CovarianceMatrix, normalize_cov
+from sunbird.abacus_utils.read_statistics import read_statistic, read_parameters
 import sys
 
 DATA_PATH = Path(__file__).parent.parent.parent / "data/different_hods/"
@@ -21,7 +22,8 @@ class Inference(ABC):
         covariance_matrix: np.array,
         priors: Dict,
         fixed_parameters: Dict[str, float],
-        filters: Dict,
+        select_filters: Dict,
+        slice_filters: Dict,
         output_dir: Path,
         device: str ="cpu",
     ):
@@ -36,7 +38,8 @@ class Inference(ABC):
         self.fixed_parameters = fixed_parameters
         self.device = device
         self.param_names = list(self.priors.keys())
-        self.filters = filters
+        self.select_filters = select_filters
+        self.slice_filters = slice_filters
         self.output_dir = Path(output_dir)
 
     @classmethod
@@ -47,12 +50,14 @@ class Inference(ABC):
     )->"Inference":
         with open(path_to_config, "r") as f:
             config = yaml.safe_load(f)
-        filters = config['filters']
+        select_filters = config['select_filters']
+        slice_filters = config['slice_filters']
         observation = cls.get_observation_for_abacus(
             cosmology= config['data']['cosmology'],
             hod_idx= config['data']['hod_idx'],
             statistic=config['data']['summary'],
-            filters=filters,
+            select_filters=select_filters,
+            slice_filters=slice_filters,
         )
         parameters = cls.get_parameters_for_abacus(
             cosmology= config['data']['cosmology'],
@@ -61,36 +66,24 @@ class Inference(ABC):
         fixed_parameters = {}
         for k in config['fixed_parameters']:
             fixed_parameters[k] = parameters[k]
-        cov_data = CovarianceMatrix.get_covariance_data(
-            statistic=config['data']['summary'],
-            filters=filters,
+        covariance = CovarianceMatrix(
+            statistics = [config['data']['summary']],
+            select_filters=select_filters,
+            slice_filters = slice_filters,
         )
-        cov_intrinsic = CovarianceMatrix.get_covariance_intrinsic(
-            statistic=config['data']['summary'],
-            filters=filters,
-        )
-<<<<<<< HEAD
-        cov_test = CovarianceMatrix.get_covariance_test(
-            statistic=config['data']['summary'],
-            filters=filters,
-        )
-=======
-        # cov_test = CovarianceMatrix.get_covariance_test(
-        #     statistic=config['data']['summary'],
-        #     filters=filters,
-        # )
->>>>>>> fc6f999a21809ca93e8e2ae25d82b53899de6b22
-        covariance_matrix = cov_data + cov_intrinsic
+        cov_data = covariance.get_covariance_data()
+        cov_emulator_error = covariance.get_covariance_emulator_error()
+        covariance_matrix = cov_data + cov_emulator_error
         theory_model = cls.get_theory_model(
-            config["theory_model"],
-            filters,
+            config["theory_model"], None
         )
         parameters_to_fit = [p for p in theory_model.parameters if p not in fixed_parameters.keys()]
         priors = cls.get_priors(config["priors"], parameters_to_fit)
         return cls(
             theory_model=theory_model,
             observation=observation,
-            filters=filters,
+            select_filters=select_filters,
+            slice_filters=slice_filters,
             covariance_matrix=covariance_matrix,
             fixed_parameters=fixed_parameters,
             priors=priors,
@@ -104,61 +97,16 @@ class Inference(ABC):
         cosmology: int,
         hod_idx: int,
         statistic: str,
-        filters: Dict,
+        select_filters: Dict,
+        slice_filters: Dict,
     )->np.array:
-        if statistic == 'density_split':
-            data = np.load(
-                DATA_PATH
-<<<<<<< HEAD
-                / f"clustering/density_split/same_hods/ds_cross_xi_smu_zsplit_Rs20_c{str(cosmology).zfill(3)}_ph000.npy",
-=======
-                / f"full_ap/clustering/ds/gaussian/ds_cross_xi_smu_zsplit_Rs20_c{str(cosmology).zfill(3)}_ph000.npy",
->>>>>>> fc6f999a21809ca93e8e2ae25d82b53899de6b22
-                allow_pickle=True,
-            ).item()
-            quintiles = range(5)
-        elif statistic == 'tpcf':
-            data = np.load(
-                DATA_PATH
-                / f"clustering/tpcf/same_hods/xi_smu_c{str(cosmology).zfill(3)}_ph000.npy",
-                allow_pickle=True,
-            ).item()
-        else:
-            raise ValueError(f'{statistic} is not implemented!')
-        s = data["s"]
-        multipoles = range(3)
-        data = data['multipoles']
-        data = np.mean(data,axis=1)
-        data = data[hod_idx]
-        if statistic == 'density_split':
-            data = xr.DataArray(
-                data, 
-                dims=("quintiles", "multipoles", "s"), 
-                coords={
-                "quintiles": list(quintiles),
-                "multipoles": list(multipoles),
-                "s": s,
-                },
-            )
-            return data.sel(
-                quintiles=filters['quintiles'],
-                multipoles=filters['multipoles'],
-                s=slice(filters['s_min'],filters['s_max']),
-            ).values.reshape(-1)
-        elif statistic == 'tpcf':
-            data = xr.DataArray(
-                data, 
-                dims=("multipoles", "s"), 
-                coords={
-                "multipoles": list(multipoles),
-                "s": s,
-                },
-            )
-            return data.sel(
-                multipoles=filters['multipoles'],
-                s=slice(filters['s_min'],filters['s_max']),
-            ).values.reshape(-1)
-
+        return read_statistic(
+                statistic=statistic,
+                cosmology = cosmology,
+                dataset = 'different_hods',
+                select_filters=select_filters,
+                slice_filters = slice_filters,
+            ).values[hod_idx].reshape(-1)
 
     @classmethod
     def get_parameters_for_abacus(
@@ -166,50 +114,11 @@ class Inference(ABC):
         cosmology: int,
         hod_idx: int,
     ):
-        return dict(
-            pd.read_csv(
-                DATA_PATH
-<<<<<<< HEAD
-                / f"parameters/same_hods/AbacusSummit_c{str(cosmology).zfill(3)}_hod1000.csv"
-=======
-                / f"full_ap/cosmologies/AbacusSummit_c{cosmology:03}_hod1000.csv"
->>>>>>> fc6f999a21809ca93e8e2ae25d82b53899de6b22
-            ).iloc[hod_idx]
-        )
-
-    @classmethod
-    def from_config(
-        cls,
-        path_to_config: Path,
-        device: str ="cpu",
-    )->"Inference":
-        with open(path_to_config, "r") as f:
-            config = yaml.safe_load(f)
-        filters = config['filters']
-        observation = cls.get_observation(
-            path_to_observation=config['data']["path_to_observation"],
-            filters=filters,
-        )
-        fixed_parameters = config["fixed_parameters"]
-        covariance_matrix = cls.get_covariance_data(
-            path_to_cov=config['data']["path_to_covariance_data"],
-            filters=filters,
-        )
-        theory_model = cls.get_theory_model(
-            config["theory_model"],
-            filters=filters,
-        )
-        parameters_to_fit = [p for p in theory_model.parameters if p not in fixed_parameters.keys()]
-        priors = cls.get_priors(config["priors"], parameters_to_fit)
-        return cls(
-            theory_model=theory_model,
-            observation=observation,
-            covariance_matrix=covariance_matrix,
-            fixed_parameters=fixed_parameters,
-            priors=priors,
-            output_dir = config['inference']['output_dir'],
-            device=device,
-        )
+        return read_parameters(
+            cosmology=cosmology,
+            dataset='different_hods',
+        ).iloc[hod_idx].to_dict()
+ 
 
     @classmethod
     def get_priors(cls, prior_config: Dict[str,Dict], parameters_to_fit: List[str])->Dict:
@@ -249,7 +158,7 @@ class Inference(ABC):
         if 'params' in theory_config:
             return module(**theory_config['params'], **filters)
         return module(
-            **filters,
+            #**filters,
         )
 
     @abstractmethod
@@ -298,7 +207,7 @@ class Inference(ABC):
             params[param] = dist.rvs()
         for p, v in self.fixed_parameters.items():
             params[p] = v
-        return params, self.theory_model(params, filters=self.filters)
+        return params, self.theory_model(params, select_filters=self.select_filters, slice_filters=self.slice_filters)
 
     def get_model_prediction(
         self,
@@ -309,7 +218,8 @@ class Inference(ABC):
             params[fixed_param] = self.fixed_parameters[fixed_param] 
         return self.theory_model(
             params,
-            filters=self.filters,
+            select_filters=self.select_filters,
+            slice_filters=self.slice_filters,
         )
 
     def get_model_prediction_vectorized(

sunbird/models/predictor.py
@@ -41,7 +41,7 @@ class Predictor(pl.LightningModule):
         nn_model = FCN.from_folder(path_to_model)
         nn_model.eval()
         data_dir = TRAIN_DIR.parent.parent / 'data/different_hods/training/ds/gaussian/'
-        s = np.load(data_dir / 's.npy')
+        s = np.load(TRAIN_DIR.parent.parent / 'data/s.npy')
         s = np.array(list(s) + list(s))
         summary_stats = cls.load_summary(path_to_model)
         for k, v in summary_stats.items():