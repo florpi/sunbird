7e244fcf5d967c059399654fa00e10efc6621e77
epaillas <enrique.paillas@waterloo.ca>
2023-03-08 08:42:04 -0800

    update

--- sunbird/models/optimise.sh
+++ sunbird/models/optimise.sh
@@ -11,8 +11,8 @@
 
 set -e
 export SLURM_CPU_BIND="cores"
-DS=$((SLURM_ARRAY_TASK_ID))
-# DS=0
+# DS=$((SLURM_ARRAY_TASK_ID))
+DS=4
 
 CORR=cross
-python optimise.py --abacus_dataset wideprior_AB --model_dir /global/homes/e/epaillas/pscratch/sunbird/trained_models/enrique/wideprior_AB/ds"$DS"_"$CORR" --statistic density_split_"$CORR" --normalize_outputs true --normalize_inputs true --s2_outputs False --select_quintiles "$DS" --select_multipoles 0 1 --slice_s 0.7 151 --accelerator gpu
+python optimise.py --abacus_dataset wideprior_AB --model_dir /global/homes/e/epaillas/pscratch/sunbird/trained_models/enrique/wideprior_AB/log_ds"$DS"_"$CORR" --statistic density_split_"$CORR" --normalize_outputs true --normalize_inputs true --s2_outputs False --select_quintiles "$DS" --select_multipoles 0 1 --slice_s 0.7 151 --accelerator gpu
