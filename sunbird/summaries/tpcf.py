8a95274a363c2bca666ef25739d1a1106530e8b2
Carol Cuesta <cuestalz@mit.edu>
2023-04-19 19:27:52 -0400

    inference runs again

--- sunbird/summaries/tpcf.py
+++ sunbird/summaries/tpcf.py
@@ -1,40 +1,11 @@
 from pathlib import Path
-#from sunbird.models.predictor import Predictor
 from sunbird.summaries.base import BaseSummary
-import numpy as np
-import matplotlib.pyplot as plt
 
 DEFAULT_PATH = (
-    Path(__file__).parent.parent.parent / "trained_models/best/tpcf/"
+    Path(__file__).parent.parent.parent / "trained_models/best/"
 )
 DEFAULT_DATA_PATH = Path(__file__).parent.parent.parent / "data/"
 
-class TPCF(BaseSummary):
-    def __init__(
-        self,
-        path_to_model: Path = DEFAULT_PATH,
-        path_to_data: Path = DEFAULT_DATA_PATH,
-    ):
-        super().__init__(
-            path_to_data=path_to_data,
-            path_to_model=path_to_model,
-        )
-        self.model = Predictor.from_folder(path_to_model, statistic='tpcf')
-
-    def forward(self, inputs, select_filters, slice_filters):
-        output = self.model(inputs)
-        if slice_filters is not None:
-            if "s" in slice_filters:
-                s_min = slice_filters["s"][0]
-                s_max = slice_filters["s"][1]
-                output = output[:, (self.model.s > s_min) & (self.model.s < s_max)]
-            if "multipoles" in slice_filters:
-                m_min = slice_filters["multipoles"][0]
-                m_max = slice_filters["multipoles"][1]
-                output = output.reshape((2, -1, output.shape[-1] // 2))[m_min:m_max]
-        if select_filters is not None:
-            if "multipoles" in select_filters:
-                multipoles = select_filters["multipoles"]
-                output = output.reshape(-1, 2, output.shape[-1] // 2)
-                output = output[:, multipoles].reshape(-1)
-        return output
+TPCF = BaseSummary.from_folder(
+    path_to_model = DEFAULT_PATH / "tpcf/",
+)
