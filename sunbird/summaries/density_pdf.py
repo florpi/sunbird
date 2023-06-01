faee14c8eb02cd6183da21fe2d65aefbf33f6f8b
Carol Cuesta <cuestalz@mit.edu>
2023-06-01 15:19:09 -0400

    added density pdf

---
+++ sunbird/summaries/density_pdf.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from typing import Optional
+from sunbird.summaries.base import BaseSummaryFolder
+
+DEFAULT_PATH = Path(__file__).parent.parent.parent / "trained_models/"
+DEFAULT_DATA_PATH = Path(__file__).parent.parent.parent / "data/"
+
+
+class DensityPDF(BaseSummaryFolder):
+    def __init__(
+        self,
+        dataset: str = "fixed_cosmo_bossprior",
+        loss: str = "mae",
+        n_hod_realizations: Optional[int] = None,
+        suffix: Optional[str] = None,
+        path_to_models: Path = DEFAULT_PATH,
+        path_to_data: Path = DEFAULT_DATA_PATH,
+        flax: bool = False,
+        **kwargs,
+    ):
+        super().__init__(
+            statistic="density_pdf",
+            loss=loss,
+            n_hod_realizations=n_hod_realizations,
+            suffix=suffix,
+            dataset=dataset,
+            path_to_models=path_to_models,
+            path_to_data=path_to_data,
+            flax=flax,
+        )
